a38e1d51dce597dd5a6cca65838a5971834723fd
Gal Schlezinger <gal@spitfire.co.il>
2022-07-13 15:14:29 +0300

    Make tests work

--- packages/example-app/tests/e2e/query.spec.ts
+++ packages/example-app/tests/e2e/query.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("basic test", async ({ page }) => {
-  await page.goto("http://localhost:3002");
+  await page.goto("/");
   const title = page.locator("#result");
   await expect(title).toHaveText("Hello, gal :D");
 });
