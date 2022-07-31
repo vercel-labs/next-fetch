ce8e4dabaeaddb67dc0afb2a2a7576e56ae6ade8
Gal Schlezinger <gal@spitfire.co.il>
2022-07-31 17:23:51 +0300

    split the declaration, add `mutate` function

--- packages/next-swr-endpoints/src/index.ts
+++ packages/next-swr-endpoints/src/index.ts
@@ -1,4 +1,5 @@
 import type { SWRResponse } from "swr";
+import type { SWRMutationResponse } from "swr/mutation";
 import type { NextConfig } from "next";
 
 type Parser<Into> =
@@ -9,6 +10,13 @@ export function query<T, V>(
   parser: Parser<T>,
   callback: (parsed: T) => Promise<V>
 ): (v: T) => SWRResponse<V> {
+  throw new Error("This code path should not be reached");
+}
+
+export function mutation<T, V>(
+  parser: Parser<T>,
+  callback: (parsed: T) => Promise<V>
+): SWRMutationResponse<V, any, T> {
   throw new Error("This code path should not be reached");
 }
 
@@ -29,19 +37,18 @@ export function withSwrApiEndpoints(given: NextConfig = {}): NextConfig {
   return {
     ...given,
     webpack(config, context) {
+      const serverLoader = require.resolve("./swr-server-endpoint-loader");
+      const pageLoader = require.resolve("./swr-client-endpoint-loader");
       config.module.rules.unshift({
         test: testRegex,
         issuerLayer: "api",
-        use: [
-          require.resolve("./swr-server-endpoint-loader"),
-          context.defaultLoaders.babel,
-        ],
+        use: [serverLoader, context.defaultLoaders.babel],
       });
       config.module.rules.unshift({
         test: testRegex,
         use: [
           {
-            loader: require.resolve("./swr-client-endpoint-loader"),
+            loader: pageLoader,
             options: {
               projectDir: context.dir,
               pageExtensionsRegex: testRegex,
