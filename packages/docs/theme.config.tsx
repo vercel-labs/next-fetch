da98d59019a1713badf6e4a84d3463c20d61c382
Gal Schlezinger <gal@spitfire.co.il>
2022-09-05 10:32:52 +0300

    Fix Nextra config

--- packages/docs/theme.config.tsx
+++ packages/docs/theme.config.tsx
@@ -1,4 +1,19 @@
-export default {
-  logo: <span>My Nextra Documentation</span>,
-  // ...
+import type { DocsThemeConfig } from "nextra-theme-docs";
+const config: DocsThemeConfig = {
+  logo: <strong>Next Fetch</strong>,
+  github: "https://github.com/vercel-labs/next-fetch",
+  titleSuffix: " | Next Fetch",
+  docsRepositoryBase: `https://github.com/vercel-labs/next-fetch/blob/main/packages/docs`,
+  unstable_faviconGlyph: "🕊️",
+  head: function Head() {
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Next Fetch" />
+      </>
+    );
+  },
 };
+export default config;
